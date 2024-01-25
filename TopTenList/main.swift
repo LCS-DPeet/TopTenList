628a8b83c1eab82a7a0dfb9cc945e0e9f5f97190
Danika Peet <DPeet@lcs.on.ca>
2024-01-25 14:50:26 -0500

    Initial Commit

---
+++ TopTenList/main.swift
@@ -0,0 +1,25 @@
+//
+//  main.swift
+//  TopTenList
+//
+//  Created by Danika Peet on 2024-01-25.
+//
+
+import Foundation
+
+// List of some of my favorite foods
+let danikasFavoriteFoods = ["potatoes", "hamburgers", "Salads", "mac and cheese", "slushy", "fries", "icecream", "chicken nuggets", "carrots", "Cucumber"]
+
+// print the first food
+print(danikasFavoriteFoods[0])
+
+//print the fifth food
+print(danikasFavoriteFoods[4])
+
+// print the final one ( the tenth item )
+print(danikasFavoriteFoods[9])
+
+//try to print the 11th food on my list
+print(danikasFavoriteFoods[10])
+
+
