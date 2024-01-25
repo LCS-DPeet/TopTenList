df70a49a087ae26f3de9e86440667aea2e9d7c7c
Danika Peet <DPeet@lcs.on.ca>
2024-01-25 15:02:10 -0500

    second printing example

--- TopTenList/main.swift
+++ TopTenList/main.swift
@@ -10,16 +10,7 @@ import Foundation
 // List of some of my favorite foods
 let danikasFavoriteFoods = ["potatoes", "hamburgers", "Salads", "mac and cheese", "slushy", "fries", "icecream", "chicken nuggets", "carrots", "Cucumber"]
 
-// count from 0 to 10
-for i in 0...9 {
-        print(i)
+// print each food name
+for food in danikasFavoriteFoods {
+    print(food)
 }
-
-//print a divider
-print("-------------")
-
-// print each food name using a loop
-for i in 0...9 {
- print(danikasFavoriteFoods[i])
-}
-
